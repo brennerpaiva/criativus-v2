6fc1938bb32cbb0f01cf76a72bdeda05835c3fec
Brenner Paiva <brennerpaiva@gmail.com>
2024-05-23 18:49:14 -0300

    testes com componentes

--- src/app/(users)/dashboard/page.tsx
+++ src/app/(users)/dashboard/page.tsx
@@ -1,22 +1,108 @@
 'use client'
 
 import { CardAdComponent } from "@/components/business/cards/card-ad.component";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@radix-ui/react-select";
+// import { SelectDemo } from "@/components/business/filter/card-demo";
+import { FilterBarComponent } from "@/components/business/filter/filter-bar.component";
+import { MenubarDemo } from "@/components/business/filter/menubar-demo";
+import { SelectGeneric } from "@/components/business/filter/select-demo";
 
 export default function DashboardPage() {
   return (
     <div className="w-full lg:grid lg:min-h-[600px] xl:min-h-[800px]">
+      <MenubarDemo />
       <div>
-        {/* <FilterBarComponent> */}
-        {/* </FilterBarComponent> */}
+        <FilterBarComponent>
+          <SelectGeneric 
+            label="Grupo Ads" 
+            placeholder="Grupo Ads"
+            items={[
+              { value: "apple", label: "Apple" },
+              { value: "banana", label: "Banana" },
+              { value: "blueberry", label: "Blueberry" },
+              { value: "grapes", label: "Grapes" },
+              { value: "pineapple", label: "Pineapple" },
+            ]}
+            className="w-[140px]"
+          />
+          <SelectGeneric 
+            label="Grupo Ads" 
+            placeholder="Grupo Ads"
+            items={[
+              { value: "apple", label: "Apple" },
+              { value: "banana", label: "Banana" },
+              { value: "blueberry", label: "Blueberry" },
+              { value: "grapes", label: "Grapes" },
+              { value: "pineapple", label: "Pineapple" },
+            ]}
+            className="w-[140px]"
+          />
+          <SelectGeneric 
+            label="Grupo Ads" 
+            placeholder="Grupo Ads"
+            items={[
+              { value: "apple", label: "Apple" },
+              { value: "banana", label: "Banana" },
+              { value: "blueberry", label: "Blueberry" },
+              { value: "grapes", label: "Grapes" },
+              { value: "pineapple", label: "Pineapple" },
+            ]}
+            className="w-[140px]"
+          />
+        </FilterBarComponent>
       </div>
-      <div className="lg:grid lg:grid-cols-4">
-        <CardAdComponent />
-        <CardAdComponent />
-        <CardAdComponent />
-        <CardAdComponent />
-        <CardAdComponent />
+      <div className="max-w-[100%] ">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+        <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+          <div className="m-auto p-4">
+            <CardAdComponent />
+          </div>
+        </div>
       </div>
+     
     </div>
   );
 }
---
+++ src/components/business/filter/select-demo.tsx
@@ -0,0 +1,37 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface SelectGenericProps {
+  label: string;
+  items: { value: string, label: string }[];
+  placeholder?: string;
+  onValueChange?: (value: string) => void;
+  className?: string;
+}
+
+export function SelectGeneric({ label, items, placeholder = "Select an option", onValueChange, className }: SelectGenericProps) {
+return (
+  <Select onValueChange={onValueChange}>
+    <SelectTrigger className={`shadcn-select-trigger ${className}`}>
+      <SelectValue placeholder={placeholder} />
+    </SelectTrigger>
+    <SelectContent className="shadcn-select-content">
+      <SelectGroup>
+        <SelectLabel className="shadcn-select-label">{label}</SelectLabel>
+        {items.map(item => (
+          <SelectItem key={item.value} value={item.value} className="shadcn-select-item">
+            {item.label}
+          </SelectItem>
+        ))}
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+);
+}
