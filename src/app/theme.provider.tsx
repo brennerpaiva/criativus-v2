abbd762e1e80489b3869e2d30a7641260a2154f6
Brenner Paiva <brennerpaiva@gmail.com>
2025-03-17 20:29:58 -0300

    feat: define tema light como padrão

--- src/app/theme.provider.tsx
+++ src/app/theme.provider.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system">
+    <NextThemesProvider attribute="class" defaultTheme="light">
       {children}
     </NextThemesProvider>
   );
