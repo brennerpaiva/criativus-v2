96fcf83a387f08a9b281b58f42a53b7a6839bd74
Brenner Paiva <brennerpaiva@gmail.com>
2025-03-17 20:29:17 -0300

    feat(card-ads): adiciona tooltip em label de métricas

--- src/components/business/cards/card-creative.component.tsx
+++ src/components/business/cards/card-creative.component.tsx
@@ -1,47 +1,56 @@
 "use client";
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"; // Importa os componentes do tooltip
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 interface CardAdProps {
   title: string;
   imageUrl: string;
-  metrics: { label: string; value: string | number }[];
+  metrics: { label: string; value: string | number, difference?: string | number }[];
   onCardClick?: () => void; // Prop opcional para capturar clique
 }
 
 export function CardAdComponent({ title, imageUrl, metrics, onCardClick }: CardAdProps) {
   return (
-    <Card
-      // Ajuste das classes e adição de cursor-pointer
-      className={cn("w-full h-full cursor-pointer")}
-      onClick={onCardClick}
-    >
-      <CardHeader className="p-0">
-        <div className="w-full h-[240px] overflow-hidden">
-          <Image
-            src={imageUrl}
-            width={241}
-            height={241}
-            alt={title}
-            className="w-full h-full object-cover"
-          />
-        </div>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-3 p-4 overflow-hidden">
-        <div className="flex">
-          <CardTitle className="text-sm truncate">{title}</CardTitle>
-        </div>
-        <div className="flex flex-col gap-3">
-          {metrics.map((metric, index) => (
-            <CardDescription key={index} className="flex justify-between">
-              <span>{metric.label}</span>
-              <span>{metric.value}</span>
-            </CardDescription>
-          ))}
-        </div>
-      </CardContent>
-    </Card>
+    <TooltipProvider>
+      <Card className={cn("w-full h-full")} onClick={onCardClick}>
+        <CardHeader className="p-0 cursor-pointer">
+          <div className="w-full h-[240px] overflow-hidden">
+            <Image
+              src={imageUrl}
+              width={241}
+              height={241}
+              alt={title}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-3 p-4 overflow-hidden">
+          <div className="flex">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <CardTitle className="text-sm truncate">{title}</CardTitle>
+              </TooltipTrigger>
+              <TooltipContent>{title}</TooltipContent>
+            </Tooltip>
+          </div>
+          <div className="flex flex-col gap-3">
+            {metrics.map((metric, index) => (
+              <CardDescription key={index} className="flex justify-between">
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <span>{metric.label}</span>
+                  </TooltipTrigger>
+                  <TooltipContent>{metric.label}</TooltipContent>
+                </Tooltip>
+                <span>{metric.value}</span>
+              </CardDescription>
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+    </TooltipProvider>
   );
 }
