0c3b1c62670d3853c2b020404f88b0123f6f680a
Brenner Paiva <brennerpaiva@gmail.com>
2025-04-09 22:18:02 -0300

    feat(select-generic): adiciona definição de defautlValue

--- src/components/business/filter/select-demo.tsx
+++ src/components/business/filter/select-demo.tsx
@@ -14,24 +14,32 @@ interface SelectGenericProps {
   placeholder?: string;
   onValueChange?: (value: string) => void;
   className?: string;
+  defaultValue?: string; // Propriedade adicionada
 }
 
-export function SelectGeneric({ label, items, placeholder = "Select an option", onValueChange, className }: SelectGenericProps) {
-return (
-  <Select onValueChange={onValueChange}>
-    <SelectTrigger className={`shadcn-select-trigger ${className}`}>
-      <SelectValue placeholder={placeholder} />
-    </SelectTrigger>
-    <SelectContent className="shadcn-select-content">
-      <SelectGroup>
-        <SelectLabel className="shadcn-select-label">{label}</SelectLabel>
-        {items.map(item => (
-          <SelectItem key={item.value} value={item.value} className="shadcn-select-item">
-            {item.label}
-          </SelectItem>
-        ))}
-      </SelectGroup>
-    </SelectContent>
-  </Select>
-);
+export function SelectGeneric({
+  label,
+  items,
+  placeholder = "Select an option",
+  onValueChange,
+  className,
+  defaultValue,
+}: SelectGenericProps) {
+  return (
+    <Select defaultValue={defaultValue} onValueChange={onValueChange}>
+      <SelectTrigger className={`shadcn-select-trigger ${className}`}>
+        <SelectValue placeholder={placeholder} />
+      </SelectTrigger>
+      <SelectContent className="shadcn-select-content">
+        <SelectGroup>
+          <SelectLabel className="shadcn-select-label">{label}</SelectLabel>
+          {items.map((item) => (
+            <SelectItem key={item.value} value={item.value} className="shadcn-select-item">
+              {item.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
 }
